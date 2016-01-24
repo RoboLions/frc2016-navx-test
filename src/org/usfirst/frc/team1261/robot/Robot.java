87734a0819d4a3e61edecb7ae23708a7b9a860c7
Rishov Sarkar <rishov.s@prhsrobotics.com>
2016-01-24 11:49:38 -0500

    Set up project specifics like NavXMicroTestCommand

--- src/org/usfirst/frc/team1261/robot/Robot.java
+++ src/org/usfirst/frc/team1261/robot/Robot.java
@@ -1,14 +1,12 @@
 
 package org.usfirst.frc.team1261.robot;
 
+import org.usfirst.frc.team1261.robot.commands.NavXMicroTestCommand;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team1261.robot.commands.ExampleCommand;
-import org.usfirst.frc.team1261.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,11 +17,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
-    SendableChooser chooser;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -31,10 +27,10 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
+//        chooser = new SendableChooser();
+//        chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
+//        SmartDashboard.putData("Auto mode", chooser);
     }
 	
 	/**
@@ -60,7 +56,8 @@ public class Robot extends IterativeRobot {
 	 * or additional comparisons to the switch structure below with additional strings & commands.
 	 */
     public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
+//        autonomousCommand = (Command) chooser.getSelected();
+    	autonomousCommand = new NavXMicroTestCommand();
         
 		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
 		switch(autoSelected) {
--- src/org/usfirst/frc/team1261/robot/commands/NavXMicroTestCommand.java
+++ src/org/usfirst/frc/team1261/robot/commands/NavXMicroTestCommand.java
@@ -1,18 +1,15 @@
-
 package org.usfirst.frc.team1261.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-import org.usfirst.frc.team1261.robot.Robot;
-
 /**
  *
  */
-public class ExampleCommand extends Command {
+public class NavXMicroTestCommand extends Command {
 
-    public ExampleCommand() {
+    public NavXMicroTestCommand() {
         // Use requires() here to declare subsystem dependencies
-        requires(Robot.exampleSubsystem);
+        // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
